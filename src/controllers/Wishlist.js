b32ea8c60ea66645c42871917b736516572010e5
Anikey <eagleremlinh@gmail.com>
2024-01-11 01:39:08 +0700

    edit file Wishlist

--- src/controllers/Wishlist.js
+++ src/controllers/Wishlist.js
@@ -19,21 +19,15 @@ module.exports = {
     },
     post: async (req, res) => {
         const { user, name } = req.body;
-        const checkUser = await Wishlist.findOne({where: {user: user}});
-        if(checkUser){
-            if(checkUser.name.includes(name)){
-                await Wishlist.destroy({
-                    where: {
-                        name: checkUser.name,
-                        user: user
-                    }
-                })
-                res.json("DELETED SUCCESSFULLY");
-            }else{
-                const post = req.body
-                await Wishlist.create(post)
-                res.json(post)
-            }
+        const check = await Wishlist.findOne({where: {user: user, name: name}});
+        if(check){
+            const id = check.id;
+            await Wishlist.destroy({
+                where: {
+                    id: id
+                }
+            })
+            res.json("DELETED SUCCESSFULLY");
         }else{
             const post = req.body
             await Wishlist.create(post)
--- src/routers/Wishlist.js
+++ src/routers/Wishlist.js
@@ -4,6 +4,6 @@ const {validateToken} = require("../middlewares/authMiddlewares");
 const Wishlist = require("../controllers/Wishlist");
 
 router.get("/", Wishlist.get)
-router.post("/", Wishlist.post)
+router.post("/", validateToken, Wishlist.post)
 
 module.exports = router;
