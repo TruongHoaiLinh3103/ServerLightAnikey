14edfcf14b4df8a476796521b04abd652774c998
Anikey <eagleremlinh@gmail.com>
2024-02-13 00:43:08 +0700

    Model

--- src/models/index.js
+++ src/models/index.js
@@ -8,12 +8,13 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../../config/config.json')[env];
 const db = {};
+const mysql2 = require("mysql2");
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config, {dialectModule: mysql2});
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config, {dialectModule: mysql2});
 }
 
 fs
