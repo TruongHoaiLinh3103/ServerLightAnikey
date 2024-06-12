2bb8302d36dcda60d1b21580dd9dc057e466b564
Anikey <eagleremlinh@gmail.com>
2024-06-12 20:35:52 +0700

    deletePrice

--- models/Wishlist.js
+++ models/Wishlist.js
@@ -14,10 +14,6 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             allowNull: false,
         },
-        price: {
-            type: DataTypes.FLOAT,
-            allowNull: false,
-        },
         rating: {
             type: DataTypes.TINYINT,
             allowNull: false,
